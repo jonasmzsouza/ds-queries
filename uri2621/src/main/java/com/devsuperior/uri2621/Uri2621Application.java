cbb9e182342e1b2eb28debb6719ced412c0492b8
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-29 17:06:08 -0300

    feat: uri2621 - spring boot sql

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2621;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -19,6 +24,13 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		// SQL
+		List<ProductMinProjection> list = repository.search1(10, 20, "P");
+		List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** SQL RESULT:");
+		result1.forEach(m -> System.out.println(m));
+		System.out.println("\n\n");
 		
 	}
 }
--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.uri2621.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT products.name "
+			+ "FROM products "
+			+ "INNER JOIN providers ON products.id_providers = providers.id "
+			+ "WHERE products.amount BETWEEN :min and :max "
+			+ "AND LOWER(providers.name) LIKE LOWER(CONCAT(:firstCharName, '%'))")
+	List<ProductMinProjection> search1(Integer min, Integer max, String firstCharName);
 
 }
