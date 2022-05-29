a15aca9f2956929d38010f4f189cae1d47b0337d
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-29 15:24:54 -0300

    feat: uri2602 - spring boot sql

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import com.devsuperior.uri2602.repositories.CustomerRepository;
 
 @SpringBootApplication
@@ -19,6 +24,13 @@ public class Uri2602Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		// SQL
+		List<CustomerMinProjection> list = repository.search1("rs");
+		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** SQL RESULT:");
+		result1.forEach(c -> System.out.println(c));
+		System.out.println("\n\n");
 
 	}
 }
--- uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,9 +1,18 @@
 package com.devsuperior.uri2602.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT name "
+			+ "FROM customers "
+			+ "WHERE LOWER(state) = LOWER(:state)")
+	List<CustomerMinProjection> search1(String state);
 
 }
