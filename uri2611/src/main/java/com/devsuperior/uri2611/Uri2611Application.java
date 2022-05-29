e761f3857893e111a340ad812eaae28efb9b8948
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-29 16:21:35 -0300

    feat: uri2611 - spring boot sql

--- uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2611;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2611.dto.MovieMinDTO;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
 import com.devsuperior.uri2611.repositories.MovieRepository;
 
 @SpringBootApplication
@@ -20,5 +25,12 @@ public class Uri2611Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// SQL
+		List<MovieMinProjection> list = repository.search1("Action");
+		List<MovieMinDTO> result1 = list.stream().map(x -> new MovieMinDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** SQL RESULT:");
+		result1.forEach(m -> System.out.println(m));
+		System.out.println("\n\n");
+		
 	}
 }
--- uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
+++ uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
@@ -1,10 +1,19 @@
 package com.devsuperior.uri2611.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2611.entities.Movie;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+	@Query(nativeQuery = true, value = "SELECT movies.id, movies.name "
+			+ "FROM movies "
+			+ "INNER JOIN genres on movies.id_genres = genres.id "
+			+ "WHERE LOWER(genres.description) = LOWER(:genreName)")
+	List<MovieMinProjection> search1(String genreName);
 
 }
