47a28a506e7b722a2f53e9b283869236293b050b
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-30 20:47:08 -0300

    feat: uri2609 - spring boot sql

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -20,5 +25,12 @@ public class Uri2609Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// SQL
+		List<CategorySumProjection> list = repository.search1();
+		List<CategorySumDTO> result1 = list.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** SQL RESULT:");
+		result1.forEach(p -> System.out.println(p));
+		System.out.println("\n\n");
+		
 	}
 }
--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.uri2609.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2609.entities.Category;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	@Query(nativeQuery = true, value = "SELECT categories.name, SUM(products.amount) "
+			+ "FROM categories "
+			+ "INNER JOIN products ON products.id_categories = categories.id "
+			+ "GROUP BY categories.name")
+	List<CategorySumProjection> search1();
 
 }
