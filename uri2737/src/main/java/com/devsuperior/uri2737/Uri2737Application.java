22ee4c3f95d693691b3f24e924523f70d81479d6
Jonas Souza <jonasmzsouza@gmail.com>
2022-05-30 21:37:39 -0300

    feat: uri2737 - spring boot sql

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2737;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
 @SpringBootApplication
@@ -20,6 +25,12 @@ public class Uri2737Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-
+		// SQL
+		List<LawyerMinProjection> list = repository.search1();
+		List<LawyerMinDTO> result1 = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+		System.out.println("\n*** SQL RESULT:");
+		result1.forEach(p -> System.out.println(p));
+		System.out.println("\n\n");
+		
 	}
 }
--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,31 @@
 package com.devsuperior.uri2737.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
 
+	@Query(nativeQuery = true, value = "(SELECT name, customers_number AS customersNumber "
+			+ "FROM lawyers "
+			+ "WHERE customers_number = ( "
+			+ "	SELECT MAX(customers_number) "
+			+ "	FROM lawyers\n"
+			+ "))"
+			+ "UNION ALL "
+			+ "(SELECT name, customers_number "
+			+ "FROM lawyers "
+			+ "WHERE customers_number = ( "
+			+ "	SELECT MIN(customers_number) "
+			+ "	FROM lawyers "
+			+ "))"
+			+ "UNION ALL "
+			+ "(SELECT 'Average', ROUND(AVG(customers_number), 0) "
+			+ "FROM lawyers)")
+	List<LawyerMinProjection> search1();
+	
 }
